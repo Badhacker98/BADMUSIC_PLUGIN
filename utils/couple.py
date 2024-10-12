e5d3ccfc4915ff6bb7a6594951a71052a7656cc8
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-10-12 14:14:06 +0530

    Update couple.py

--- utils/couple.py
+++ utils/couple.py
@@ -3,7 +3,7 @@ from typing import Dict, Union
 from pymongo import MongoClient
 
 from config import MONGO_DB_URI
-from config import MONGO_DB_URI as DB_NAME
+from config import DB_NAME
 client = MongoClient(MONGO_DB_URI)
 db = client[DB_NAME]
 
