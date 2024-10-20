9083dd27834bbbd4ccfcbc869574b3547d8fb8fb
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-10-20 12:05:09 +0530

    Update welcome.py

--- plugins/welcome.py
+++ plugins/welcome.py
@@ -88,8 +88,8 @@ def welcomepic(user_id, user_username, user_names, chat_name, user_photo, chat_p
     user_img = Image.open(user_photo).convert("RGBA")
     chat_img = Image.open(chat_photo).convert("RGBA")
     
-    chat_img_circle = circle(chat_img, size=(240, 240), brightness_factor=1.2)
-    user_img_circle = circle(user_img, size=(232, 232), brightness_factor=1.2)
+    chat_img_circle = circle(chat_img, size=(190, 190), brightness_factor=1.2)
+    user_img_circle = circle(user_img, size=(210, 210), brightness_factor=1.2)
     
     background.paste(chat_img_circle, (270, 260), chat_img_circle)
     background.paste(user_img_circle, (827, 260), user_img_circle)
