1162a2af22a22ae90df1ab34cba789ff672a6a18
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-10-20 13:12:50 +0530

    Update welcome.py

--- plugins/welcome.py
+++ plugins/welcome.py
@@ -102,9 +102,9 @@ def welcomepic(user_id, user_username, user_names, chat_name, user_photo, chat_p
     red = (255, 255, 255)   
     red = (19, 136, 8)
 
-    draw.text((450, 447), f"Name:  {user_names}", fill=red, font=font)
-    draw.text((450, 487), f"User Id:  {user_id}", fill=red, font=font)
-    draw.text((450, 520), f"Username:  {user_username}", fill=red, font=font)
+    draw.text((450, 443), f"Name:  {user_names}", fill=red, font=font)
+    draw.text((450, 483), f"User Id:  {user_id}", fill=red, font=font)
+    draw.text((450, 515), f"Username:  {user_username}", fill=red, font=font)
     
     background.save(f"downloads/welcome#{user_id}.png")
     return f"downloads/welcome#{user_id}.png"
